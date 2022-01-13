1923a9938d184fc2471867ef7cf53eb8c2349b80
Thayrone Lião da Silva <76060404+Thayrone-L@users.noreply.github.com>
2022-01-13 17:04:49 -0300

    exercicios e funções da aula 05

--- Aula05/exAula05.php
+++ Aula05/exAula05.php
@@ -11,8 +11,16 @@
     <div>
     <?php
 
-    echo "Banana";
-
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+        $m = ($n1+$n2)/2;
+        echo "Valor de n1: ".$n1." e Valor de n2: ".$n2;
+        echo "<br/>A soma vale: ".($n1+$n2);
+        echo "<br/>A subtração vale: ".($n1-$n2);
+        echo "<br/>A multiplicação vale: ".($n1*$n2);
+        echo "<br/>A Divisão vale: ".($n1/$n2);
+        echo "<br/>O modulo vale: ".($n1%$n2);
+        echo "<br/>A media vale: ".($m);
 
     ?>
     </div>
---
+++ Aula05/funcoesAritimeticas.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="_css/estilo.css"/>
+    <title>Fuções aritmeticas</title>
+</head>
+<body>
+    <div>
+    <?php
+
+    $n1 = 7;
+    $n2 = 5;
+    $s = $n1+$n2;
+    echo "O resultado é: $s";
+
+
+    ?>
+    </div>
+</body>
+</html>
